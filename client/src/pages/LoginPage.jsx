28958d889461d973bd0a66e65129ab524fa8e34c
Rahul kumar <rahulkumarsah269@gmail.com>
2024-07-31 14:42:21 +0530

    Update index.js

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -62,7 +62,7 @@ const LoginPage = () => {
           />
           <button type="submit">LOG IN</button>
         </form>
-        <a href="/register">Don't have an account? Sign In Here</a>
+        <a href="/register">Don't have an account? Sign Up Here</a>
       </div>
     </div>
   );
